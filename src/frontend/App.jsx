da567e3a58e029c78c0796a937ea33041c2dc595
Mariana Belo <101350371+MarianaBelo26@users.noreply.github.com>
2025-04-10 14:56:43 -0300

    update App.jsx

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import {useTranslation} from 'react-i18next'
 import './i18n'
 import Nav from './pages/HomePage/Nav'
